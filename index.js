7701b84aa59d086a146a94a046e2a85521f824e1
OmThakur92 <omkumarbhu@gmail.com>
2021-09-03 01:45:42 +0530

    Added the files

---
+++ index.js
@@ -0,0 +1,68 @@
+var len = document.querySelectorAll(".drum").length;
+
+//Detecting the button press
+for (var i = 0; i < len; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var innerCon = this.innerHTML;
+
+    pressDrum(innerCon);
+    buttonAnimation(innerCon);
+  });
+}
+
+//Detecting the keyboard press
+document.addEventListener("keypress", function (event) {
+  pressDrum(event.key);
+  buttonAnimation(event.key);
+});
+
+function pressDrum(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(this);
+  }
+}
+
+function buttonAnimation(key) {
+  var change = document.querySelector("." + key);
+  change.classList.add("pressed");
+
+  setTimeout(function () {
+    change.classList.remove("pressed");
+  }, 200);
+}
